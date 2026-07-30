a3a45af7f8f7a57775d495b6b2f3a078ce1adb99
yashvc1111-droid <yashvc1111@gmail.com>
2026-07-30 19:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (85.52%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -2,25 +2,24 @@ class Solution {
     public int search(int[] nums, int target) {
 
         int start = 0;
-        int end = nums.length - 1;
+        int end = nums.length -1;
 
         while(start <= end){
-            
+
             int mid = start + (end - start) / 2;
 
             if (target < nums[mid]){
-                end = mid -1;
+                end = mid - 1;
             }
             else if (target > nums[mid]){
-                start = mid + 1;
+
+                start  = mid + 1;
             }
 
-            if (target == nums[mid]){
+            else {
                 return mid;
             }
-        
         }
-        return -1;
-        
+        return -1; 
     }
 }
